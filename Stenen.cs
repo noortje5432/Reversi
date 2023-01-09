20cae07ce928fdc8eef5372aff14cdae3f6c7a8a
NoortjeP <55238553+NoortjeP@users.noreply.github.com>
2023-01-09 14:04:52 +0100

    b

--- Stenen.cs
+++ Stenen.cs
@@ -16,18 +16,18 @@ namespace Reversi_Namespace
         
         public Stenen(double KleurX, double KleurY, int KleurVanDeSteen)
         {
-            this.KleurX = Form1.zoomx() * KleurX;
-            this.KleurY = Form1.zoomy() * Kleur;
+            this.KleurX = Form1.schaalx() * KleurX;
+            this.KleurY = Form1.schaaly() * Kleur;
             this.KleurVanSteen = KleurVanDeSteen; 
         }
 
         public void tekenSteen(object o, PaintEventArgs pea) 
         {
-            float maatx = (float)Form1.zoomx() - 10;
-            float maaty = (float)Form1.zoomy() - 10;
-            if (kleurSteen == 0)
+            float maatx = (float)Form1.schaalx() - 10;
+            float maaty = (float)Form1.schaaly() - 10;
+            if (KleurVanSteen == 0)
                 Kleur = Brushes.RoyalBlue;
-            if (kleursteen == 1)
+            if (KleurVanSteen == 1)
                 Kleur = Brushes.DarkRed;
             pea.Graphics.FillEllipse(Kleur, (float)KleurX + 5, (float)KleurY, maatx, maaty);
         }
