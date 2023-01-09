28324581dfd8b7f4aac62ae0bcf05811e2d49b4d
NoortjeP <55238553+NoortjeP@users.noreply.github.com>
2023-01-09 13:54:23 +0100

    v

--- Stenen.cs
+++ Stenen.cs
@@ -1,13 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace Reversi_Namespace
 {
-    internal class Stenen
+    class Stenen
     {
+        Brush Kleur;
+        public int KleurVanSteen;
+        double KleurX, KleurY; 
+        
+        public Stenen(double KleurX, double KleurY, int KleurVanDeSteen)
+        {
+            this.KleurX = Scherm.zoomx() * KleurX;
+            this.KleurY = Scherm.zoomy() * Kleur;
+            this.KleurVanDeSteen = KleurVanDeSteen;
+        }
 
+        public void tekenSteen(object o, PaintEventArgs pea) 
+        {
+            float maatx = (float)Scherm.zoomx() - 10;
+            float maaty = (float)Scherm.zoomy() - 10;
+            if (kleurSteen == 0)
+                Kleur = Brushes.RoyalBlue;
+            if (kleursteen == 1)
+                Kleur = Brushes.Red;
+            pea.Graphics.FillEllipse(Kleur, (float)KleurX + 5, (float)KleurY, maatx, maaty);
+        }
     }
 }
